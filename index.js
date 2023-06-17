2199fa5cbec0b4ab4988ff62e85ad3147df4e065
TxemaN <txalcala@hotmail.com>
2023-06-17 13:26:25 +0200

    casi hecho

---
+++ index.js
@@ -0,0 +1,50 @@
+//Variable sin valor//
+let variableSinValor;
+//Crear booleanos 1 y 2 con valor booleano//
+let booleano1 = true;
+let booleano2 = true;
+//variabnlel constante pi//
+const PI = 3.14;
+//constante TAU//
+const TAU = PI*2;
+//comparar boolean1 y boolean2//
+let booleanoAnd = booleano1==booleano2 ? true: false;
+//booleano not para booleano1//
+let booleanoNot = !booleano1;
+//booleanoMix0 comparado//
+let booleanoMix0 = booleano1||booleano2==booleano1||!booleano1 && !booleano2;
+//OPERADORES incrementar despues//
+let incrementarDesp = 2;
+let resultadoDesp = incrementarDesp++;
+//Incrementar antes//
+let incrementarAntes = 2;
+let resultadoAntes = ++incrementarAntes;
+//BUCLES hasta 10//
+for(contarHasta10_2 = 0; contarHasta10_2 <10; contarHasta10_2++);
+//postI y postJ//
+postI=0;
+postJ=0;
+do {
+    postI = postI + postJ;
+    postJ++;
+  } while (postJ < 11);
+  //pares e impares//
+  let sumaPares = 0;
+  for (i=0; i < 10; i++) 
+if ( i%2==0) { sumaPares = sumaPares+i;}
+
+//ITERACIONES PROYECTO INDIVIDUAL variable valor numerico//
+let variableValorNumerico = 5;
+//nombre//
+const MiNombre = "Txema";
+//Num favorito//
+const MiNumeroFav = 7;
+//BOOLEANOS//
+var booleanoOr = booleano1==booleano2 ? true:false;
+//booleanomix1//
+var booleanoMix1 = booleano1&&TAU/2==PI||variableValorNumerico>=MiNumeroFav ? true:false;
+//seisnoesnueve//
+var seisNoEsNueve = 6=== !9 ? false: true;
+//booleanomix2//
+let booleanoMix2 = variableValorNumerico > 0|| variableValorNumerico < (MiNumeroFav * TAU);
+  
